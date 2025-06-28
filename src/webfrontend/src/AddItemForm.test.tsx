bcb7154626c53223332bdb09a0940b4cda7533e7
brucengdev <brucengdev@gmail.com>
2025-06-28 15:53:55 +0700

    wip

--- src/webfrontend/src/AddItemForm.test.tsx
+++ src/webfrontend/src/AddItemForm.test.tsx
@@ -1,10 +1,12 @@
-import { render } from "@testing-library/react";
-import {describe, it} from 'vitest'
+import { render, screen } from "@testing-library/react";
+import {describe, expect, it} from 'vitest'
 import '@testing-library/jest-dom'
 import { AddItemForm } from "./AddItemForm";
 
 describe("AddItemForm", () => {
     it("has necessary ui components", () => {
         render(<AddItemForm />)
+
+        expect(screen.getByRole("heading", {name: "New item"})).toBeInTheDocument()
     })
 })
--- src/webfrontend/src/AddItemForm.tsx
+++ src/webfrontend/src/AddItemForm.tsx
@@ -1,5 +1,8 @@
 
 export function AddItemForm() {
     return <div data-testid="add-item-form">
+        <h1>
+            New item
+        </h1>
     </div>
 }
