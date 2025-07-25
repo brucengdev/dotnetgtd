e622fc9433cb00e54192e70162f76e69915bc5d5
brucengdev <brucengdev@gmail.com>
2025-07-25 18:17:08 +0700

    Update the database, add Item table.

--- src/Backend.WebApi/GTDContext.cs
+++ src/Backend.WebApi/GTDContext.cs
@@ -5,6 +5,7 @@ namespace Backend.Models
     public class GTDContext: DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Item> Items { get; set; }
 
         public GTDContext(DbContextOptions<GTDContext> options)
             : base(options)
