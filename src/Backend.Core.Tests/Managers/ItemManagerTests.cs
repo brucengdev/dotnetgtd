a88d5f66802986add0e1b3b59010435a22f7df20
brucengdev <brucengdev@gmail.com>
2025-07-25 17:45:48 +0700

    wip

---
+++ src/Backend.Core.Tests/Managers/ItemManagerTests.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Core.Manager;
+
+namespace Backend.Core.Tests;
+
+public class ItemManagerTests
+{
+    [Fact]
+    public void Creating_item_is_successful()
+    {
+        //arrange
+        var sut = new ItemManager();
+
+        //act
+
+        //assert
+    }
+}
---
+++ src/Backend.Core/Manager/ItemManager.cs
@@ -0,0 +1,11 @@
+﻿using Backend.Models;
+
+namespace Backend.Core.Manager;
+
+public class ItemManager: IItemManager
+{
+    public int CreateItem(Item item, int userId)
+    {
+        throw new NotImplementedException();
+    }
+}
