091f18efa3b3bf5667bb3ed720e903917f6c4232
brucengdev <brucengdev@gmail.com>
2025-07-26 08:58:11 +0700

    wip

--- src/webfrontend/src/ItemList.test.tsx
+++ src/webfrontend/src/ItemList.test.tsx
@@ -2,10 +2,16 @@ import { screen, render } from "@testing-library/react";
 import {describe, expect, it} from 'vitest'
 import '@testing-library/jest-dom'
 import ItemList from "./ItemList";
+import { TestClient } from "./__test__/TestClient";
 
 describe("ItemList", () => {
     it("shows message when there are no items", () => {
         render(<ItemList />)
         expect(screen.getByText("There are no items.")).toBeInTheDocument()
+    })
+
+    it("shows a list of items", () => {
+        const testClient = new TestClient()
+        render(<ItemList client={testClient} />)
     })
 })
--- src/webfrontend/src/ItemList.tsx
+++ src/webfrontend/src/ItemList.tsx
@@ -1,4 +1,10 @@
-export default function ItemList() {
+import { IClient } from "./api/Client"
+
+interface ItemListProps {
+    client?: IClient
+}
+
+export default function ItemList(_: ItemListProps) {
     return <div data-testId="item-list">
         There are no items.
     </div>
