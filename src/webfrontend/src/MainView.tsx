7ce661ff87d2f173ec18fa152abc977962d808e4
brucengdev <brucengdev@gmail.com>
2025-07-27 11:31:14 +0700

    wip

--- src/webfrontend/src/MainView.tsx
+++ src/webfrontend/src/MainView.tsx
@@ -3,6 +3,7 @@ import { IClient } from "./api/Client"
 import { Button } from "./controls/Button"
 import { AddItemForm } from "./AddItemForm"
 import ItemList from "./ItemList"
+import { Item } from "./models/Item"
 
 export interface MainViewProps {
   client: IClient
@@ -11,11 +12,16 @@ export interface MainViewProps {
 
 export function MainView({onLogout, client} : MainViewProps) {
     const [showNewTaskForm, setShowNewTaskForm] = useState(false)
+    const [items, setItems] = useState(undefined as (Item[]|undefined))
+    if(items === undefined) {
+        client.GetItems()
+            .then(items => setItems(items))
+    }
     return <div data-testid="main-view" className="row-auto">
       <div className="mx-auto max-w-2xl text-center">
             <h2 className="text-2xl font-semibold text-gray-900">GTD</h2>
       </div>
-      <ItemList client={client} />
+      <ItemList items={items} />
       {showNewTaskForm
         ? <AddItemForm client={client} 
             onCancel={() => setShowNewTaskForm(false)} 
