6b24303d341265db2d7d3d203f5e3ae990f24c24
brucengdev <brucengdev@gmail.com>
2025-08-02 15:15:21 +0700

    wip

---
+++ src/webfrontend/src/ItemView.test.tsx
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest"
+import ItemView from "./ItemView"
+import { screen, render } from "@testing-library/react";
+import '@testing-library/jest-dom'
+
+describe("ItemView", () => {
+    it("renders description", () => {
+        render(<ItemView description="Test Description" />)
+
+        expect(screen.getByTestId("description")).toBeInTheDocument()
+    })
+})
