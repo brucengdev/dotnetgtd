257fed287e617cdb87b98d7a6a32b154dd3ad9dc
brucengdev <brucengdev@gmail.com>
2025-08-02 15:45:43 +0700

    wip

--- src/webfrontend/src/ItemList.test.tsx
+++ src/webfrontend/src/ItemList.test.tsx
@@ -1,8 +1,9 @@
-import { screen, render } from "@testing-library/react";
-import {describe, expect, it} from 'vitest'
+import { screen, render, fireEvent } from "@testing-library/react";
+import {describe, expect, it, vitest} from 'vitest'
 import '@testing-library/jest-dom'
 import ItemList from "./ItemList";
 import { sleep } from "./__test__/testutils";
+import { Item } from "./models/Item";
 
 describe("ItemList", () => {
     it("shows message when there are no items", () => {
@@ -38,5 +39,29 @@ describe("ItemList", () => {
         let itemViews = screen.getAllByTestId("item")
         expect(itemViews.length).toBe(1)
         expect(itemViews[0].querySelector('[data-testid="description"]')?.textContent).toBe("Task C")
+    })
+
+    it("executes onDelete when an item is deleted by user", async () => {
+        const items = [
+            { description: "Task A" },
+            { description: "Task B" },
+            { description: "Task C" }
+        ]
+        let selectedItem: Item | undefined = undefined
+        const onDelete = vitest.fn((item: Item) => {
+            selectedItem = item
+        })
+        render(<ItemList items={items} onDelete={onDelete} />)
+        await sleep(10)
+
+        //get the delete button for the second item
+        const deleteButtons = screen.getAllByRole("button", { name: "Delete" })
+        expect(deleteButtons.length).toBe(3)
+        const deleteButton = deleteButtons[1]
+        fireEvent.click(deleteButton)
+
+        fireEvent.click(screen.getByRole("button", { name: "Yes" }))
+        expect(onDelete).toHaveBeenCalled()
+        expect(selectedItem).toEqual({ description: "Task B" })
     })
 })
