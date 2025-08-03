1c2ec78ce06f990b8c3b5757025bd99798c506f8
brucengdev <brucengdev@gmail.com>
2025-08-03 10:26:27 +0700

    wip

--- src/Backend.Core.Tests/Managers/ItemManagerTests.CreateItem.cs
+++ src/Backend.Core.Tests/Managers/ItemManagerTests.CreateItem.cs
@@ -46,38 +46,4 @@ public partial class ItemManagerTests
             UserId = expectedUserId
         });
     }
-    
-    [Fact]
-    public void GetItems_is_successful()
-    {
-        //arrange
-        var repo = new TestItemRepository();
-        repo.Items.Add(new Item
-        {
-            Id = 1,
-            Description = "Task 1",
-            UserId = 123
-        });
-        repo.Items.Add(new Item
-        {
-            Id = 2,
-            Description = "Task 2",
-            UserId = 234
-        });
-        var sut = new ItemManager(repo);
-        var expectedUserId = 123;
-
-        //act
-        var items = sut.GetItems(expectedUserId);
-
-        //assert
-        items.ShouldBe(new List<Item>()
-        {
-            new () {
-                Id = 1,
-                Description = "Task 1",
-                UserId = 123
-            }
-        });
-    }
 }
---
+++ src/Backend.Core.Tests/Managers/ItemManagerTests.GetItems.cs
@@ -0,0 +1,43 @@
+﻿using Backend.Core.Manager;
+using Backend.Core.Tests.Mocks;
+using Backend.Models;
+using Shouldly;
+
+namespace Backend.Core.Tests;
+
+public partial class ItemManagerTests
+{
+    [Fact]
+    public void GetItems_is_successful()
+    {
+        //arrange
+        var repo = new TestItemRepository();
+        repo.Items.Add(new Item
+        {
+            Id = 1,
+            Description = "Task 1",
+            UserId = 123
+        });
+        repo.Items.Add(new Item
+        {
+            Id = 2,
+            Description = "Task 2",
+            UserId = 234
+        });
+        var sut = new ItemManager(repo);
+        var expectedUserId = 123;
+
+        //act
+        var items = sut.GetItems(expectedUserId);
+
+        //assert
+        items.ShouldBe(new List<Item>()
+        {
+            new () {
+                Id = 1,
+                Description = "Task 1",
+                UserId = 123
+            }
+        });
+    }
+}
