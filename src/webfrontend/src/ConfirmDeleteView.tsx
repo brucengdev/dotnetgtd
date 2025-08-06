fb5cca56910e069545196e3f2cdb615c1cb0ebac
brucengdev <brucengdev@gmail.com>
2025-08-06 07:47:55 +0700

    wip

--- src/webfrontend/src/ConfirmDeleteView.tsx
+++ src/webfrontend/src/ConfirmDeleteView.tsx
@@ -6,11 +6,11 @@ interface ConfirmDeleteViewProps {
 }
 
 export function ConfirmDeleteView({onYes, onNo}: ConfirmDeleteViewProps) {
-    return <div data-testid="confirmDeleteView">
-        <h2>Confirm to delete?</h2>
-        <div>
-            <Button text="Yes" mode={ButtonMode.DANGER} onClick={() => {if(onYes) {onYes()}}}  />
-            <Button text="No" mode={ButtonMode.PRIMARY} onClick={() => {if(onNo) {onNo()}}}  />
-        </div>
+    return <div data-testid="confirmDeleteView" className="grid-cols-3">
+        <h2 className="font-semibold text-red-600 text-2xl mb-2">
+            Confirm to delete?
+        </h2>
+        <Button text="Yes" className="mr-2" mode={ButtonMode.DANGER} onClick={() => {if(onYes) {onYes()}}}  />
+        <Button text="No" mode={ButtonMode.PRIMARY} onClick={() => {if(onNo) {onNo()}}}  />
     </div>
 }
--- src/webfrontend/src/ItemView.tsx
+++ src/webfrontend/src/ItemView.tsx
@@ -9,10 +9,15 @@ interface ItemViewProps {
 export default function ItemView(props: ItemViewProps) {
     const { description, onDelete } = props
     const [ showConfirmDelete, setShowConfirmDelete ] = useState(false)
-    return <div data-testId="item" className="grid grid-cols-2 mb-1">
-                <div data-testId="description">{description}</div>
-                {showConfirmDelete
-                    ?<ConfirmDeleteView onNo={() => setShowConfirmDelete(false)} onYes={onDelete} />
-                    : <Button text="Delete" className="justify-self-end" mode={ButtonMode.DANGER} onClick={() => setShowConfirmDelete(true)} />}
+    return <div data-testId="item">
+        <div  className="grid grid-cols-2 mb-1">
+            <div data-testId="description">{description}</div>
+            {showConfirmDelete
+                ? <></>
+                : <Button text="Delete" className="justify-self-end" mode={ButtonMode.DANGER} onClick={() => setShowConfirmDelete(true)} />}
+        </div>
+            {showConfirmDelete
+                ?<ConfirmDeleteView onNo={() => setShowConfirmDelete(false)} onYes={onDelete} />
+                : <></>}
         </div>
 }
