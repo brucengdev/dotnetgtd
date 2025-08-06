4a442be1cc8d269160acf389ba6fd60180b6af42
brucengdev <brucengdev@gmail.com>
2025-08-06 07:39:49 +0700

    wip

--- src/webfrontend/src/ConfirmDeleteView.tsx
+++ src/webfrontend/src/ConfirmDeleteView.tsx
@@ -1,18 +1,16 @@
+import { Button, ButtonMode } from "./controls/Button"
+
 interface ConfirmDeleteViewProps {
     onYes?: () => void
     onNo?: () => void
 }
 
 export function ConfirmDeleteView({onYes, onNo}: ConfirmDeleteViewProps) {
-    return <div data-testid="confirmDeleteView" className="row">
-        <h2 className="row">Confirm to delete?</h2>
-        <div className="row">
-            <button 
-                className="btn btn-danger col-1"
-                onClick={() => {if(onYes) {onYes()}}} >Yes</button>
-            <button 
-                className="btn btn-primary col-1"
-                onClick={() => {if(onNo) {onNo()}}} >No</button>
+    return <div data-testid="confirmDeleteView">
+        <h2>Confirm to delete?</h2>
+        <div>
+            <Button text="Yes" mode={ButtonMode.DANGER} onClick={() => {if(onYes) {onYes()}}}  />
+            <Button text="No" mode={ButtonMode.PRIMARY} onClick={() => {if(onNo) {onNo()}}}  />
         </div>
     </div>
 }
