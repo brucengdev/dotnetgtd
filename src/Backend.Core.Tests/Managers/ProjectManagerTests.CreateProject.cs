637a463d0e6c52bdc7ecd43cfe996ee4e7ab3c2f
brucengdev <brucengdev@gmail.com>
2025-08-16 18:52:12 +0700

    wip

---
+++ src/Backend.Core.Tests/Managers/ProjectManagerTests.CreateProject.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Core.Manager;
+
+namespace Backend.Core.Tests;
+
+public partial class ProjectManagerTests
+{
+    [Fact]
+    public void Create_project_must_be_successful()
+    {
+        //arrange
+        var sut = new ProjectManager();
+    } 
+}
