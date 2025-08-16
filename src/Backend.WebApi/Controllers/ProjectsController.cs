98e8ce9ffb37256c0cc9a356c5925b13858bacdb
brucengdev <brucengdev@gmail.com>
2025-08-16 07:04:52 +0700

    wip

---
+++ src/Backend.WebApi/Controllers/ProjectsController.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Core.Manager;
+using Backend.Models;
+using Backend.WebApi.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectsController: ControllerBase
+    {
+    }
+}
