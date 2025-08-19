7e6d8101cc1005bfc6a6588ac7de6e9f0f62f294
brucengdev <brucengdev@gmail.com>
2025-08-19 08:13:14 +0700

    wip

--- src/webfrontend/src/ProjectListItem.tsx
+++ src/webfrontend/src/ProjectListItem.tsx
@@ -2,7 +2,7 @@ interface ProjectListItemProps {
     name: string
 }
 export function ProjectListItem({name}: ProjectListItemProps) {
-    return <div data-testid="project">
+    return <div data-testid="project"  className="grid grid-cols-2 mb-1">
         <div data-testid="description">{name}</div>
     </div>
 }
