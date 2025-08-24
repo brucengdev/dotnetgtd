625e1ca1a0bd1175081a18132ccf8a0b66ccb45a
brucengdev <brucengdev@gmail.com>
2025-08-24 15:17:43 +0700

    wip

--- src/webfrontend/src/ProjectList.test.tsx
+++ src/webfrontend/src/ProjectList.test.tsx
@@ -13,5 +13,21 @@ describe("ProjectList", () => {
         
         const projectItems = screen.getAllByTestId("project")
         expect(projectItems.length).toBe(2)
+
+        const projectA = projectItems[0]
+        expect(projectA.querySelector("[data-testId='name']")?.textContent)
+            .toBe('Project A')
+
+        const projectB = projectItems[0]
+        expect(projectB.querySelector("[data-testId='name']")?.textContent)
+            .toBe('Project B')
+    })
+
+    it("calls onDelete when a project is deleted", () => {
+        const projects = [
+            { id: 1, name: "Project A" },
+            { id: 2, name: "Project B" }
+        ]
+        render(<ProjectList projects={projects} />)
     })
 });
