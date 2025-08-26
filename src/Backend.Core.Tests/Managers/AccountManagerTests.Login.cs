ac472c3453588079d176eceb373569379a9acb49
brucengdev <brucengdev@gmail.com>
2025-08-26 08:33:27 +0700

    wip

--- src/Backend.Core.Tests/Managers/AccountManagerTests.Login.cs
+++ src/Backend.Core.Tests/Managers/AccountManagerTests.Login.cs
@@ -15,7 +15,8 @@ namespace Backend.Core.Tests
             userRepo.AddUser(new User
             {
                 Username = "johndoe",
-                Password = "testPassword"
+                Password = "testPassword",
+                PasswordHash = HashPassword("testPassword")
             });
             var sut = new AccountManager(userRepo);
             
