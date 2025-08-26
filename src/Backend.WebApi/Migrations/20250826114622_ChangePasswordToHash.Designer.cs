23be9414c8e1747922ccb8aa8ce1108942b3d755
brucengdev <brucengdev@gmail.com>
2025-08-26 20:06:16 +0700

    Change Password field to PasswordHash.

--- src/Backend.WebApi/Migrations/20250826114622_ChangePasswordToHash.Designer.cs
+++ src/Backend.WebApi/Migrations/20250826114622_ChangePasswordToHash.Designer.cs
@@ -3,6 +3,7 @@ using Backend.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.WebApi.Migrations
 {
     [DbContext(typeof(GTDContext))]
-    partial class GTDContextModelSnapshot : ModelSnapshot
+    [Migration("20250826114622_ChangePasswordToHash")]
+    partial class ChangePasswordToHash
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
