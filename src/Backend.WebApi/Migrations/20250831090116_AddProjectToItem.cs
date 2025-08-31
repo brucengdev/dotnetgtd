3e870892743f204cbbe98eac52742806ff647c99
brucengdev <brucengdev@gmail.com>
2025-08-31 16:03:22 +0700

    Added migration.

---
+++ src/Backend.WebApi/Migrations/20250831090116_AddProjectToItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectToItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectId",
+                table: "Items",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProjectId",
+                table: "Items");
+        }
+    }
+}
