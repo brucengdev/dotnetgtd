c25fed6f5a04440d352dd6b773bfa8afc260a5dc
brucengdev <brucengdev@gmail.com>
2025-08-31 16:00:18 +0700

    wip

--- src/Backend.Core/Models/Item.cs
+++ src/Backend.Core/Models/Item.cs
@@ -6,7 +6,7 @@ public class Item
     public string Description { get; set; }
     public int UserId { get; set; }
     
-    public int ProjectId { get; set; }
+    public int? ProjectId { get; set; }
 
     public override bool Equals(object? obj)
     {
