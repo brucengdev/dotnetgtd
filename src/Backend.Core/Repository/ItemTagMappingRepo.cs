1027a4c477a5a5deae6625d8cbc38065274cf3ea
brucengdev <brucengdev@gmail.com>
2025-09-01 22:06:22 +0700

    wip

---
+++ src/Backend.Core/Repository/ItemTagMappingRepo.cs
@@ -0,0 +1,11 @@
+﻿using Backend.Models;
+
+namespace Backend.Core.Repository;
+
+public class ItemTagMappingRepo: IItemTagMappingRepo
+{
+    public int CreateMapping(ItemTagMapping mapping)
+    {
+        throw new NotImplementedException();
+    }
+}
