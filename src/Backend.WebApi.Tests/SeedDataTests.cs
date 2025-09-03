70ad609911507460987edd4a3cf8e9ef10b00f35
brucengdev <brucengdev@gmail.com>
2025-09-03 12:34:07 +0700

    wip

--- src/Backend.WebApi.Tests/SeedDataTests.cs
+++ src/Backend.WebApi.Tests/SeedDataTests.cs
@@ -9,7 +9,7 @@ namespace Backend.WebApi.Tests;
 public class SeedDataTests
 {
     [Fact]
-    public void Must_add_admin_user_with_specified_salt()
+    public void Must_add_admin_user_with_specified_salt_and_password()
     {
         //arrange
         var dbContextOptionsBuilder = new DbContextOptionsBuilder<GTDContext>();
@@ -17,15 +17,16 @@ public class SeedDataTests
         var contextOptions = dbContextOptionsBuilder.Options;
         var context = new GTDContext(contextOptions);
         var salt = "abcdef";
+        var adminPassword = "adminPassword";
         
         //act
-        SeedData.Initialize(context, salt);
+        SeedData.Initialize(context, adminPassword, salt);
         
         //assert
         context.Users.Count().ShouldBe(1);
         var admin = context.Users.Single();
         admin.Username.ShouldBe("admin");
-        admin.PasswordHash.ShouldBe(CreateHash("admin", salt));
+        admin.PasswordHash.ShouldBe(CreateHash(adminPassword, salt));
     }
 
     private string CreateHash(string input, string salt)
--- src/Backend.WebApi/Constants.cs
+++ src/Backend.WebApi/Constants.cs
@@ -4,4 +4,5 @@ public static class Constants
 {
     public const string USER_ID = "UserId";
     public const string DEFAULT_HASH_SALT = "Ax4663akaa";
+    public const string DEFAULT_ADMIN_PASSWORD = "admin";
 }
