59a5c54988bad864fd7609278c045801c3808f04
brucengdev <brucengdev@gmail.com>
2025-09-04 08:03:13 +0700

    Show later status on item view.

--- src/webfrontend/src/ItemView.tsx
+++ src/webfrontend/src/ItemView.tsx
@@ -11,20 +11,24 @@ interface ItemViewProps {
     later: boolean
 }
 export default function ItemView(props: ItemViewProps) {
-    const { description, onDelete, done } = props
+    const { description, onDelete, done, later } = props
     const [ showConfirmDelete, setShowConfirmDelete ] = useState(false)
     return <div data-testId="item">
         <div  className="grid grid-cols-4 mb-1">
             <div data-testId="description">{description}</div>
             <div data-testId="project">{props.projectName??""}</div>
             <div data-testId="tags">{props.tagNames?.join(",") ?? ""}</div>
-            <label>Done<input type="checkbox" checked={done}></input></label>
+            <label>Done<input type="checkbox" checked={done} /></label>
+            <label>Later<input type="checkbox" checked={later} /></label>
             {showConfirmDelete
                 ? <></>
-                : <Button text="Delete" className="justify-self-end" mode={ButtonMode.DANGER} onClick={() => setShowConfirmDelete(true)} />}
+                : <Button text="Delete" className="justify-self-end" 
+                    mode={ButtonMode.DANGER} 
+                    onClick={() => setShowConfirmDelete(true)} />}
         </div>
             {showConfirmDelete
-                ?<ConfirmDeleteView onNo={() => setShowConfirmDelete(false)} onYes={onDelete} />
+                ?<ConfirmDeleteView onNo={() => setShowConfirmDelete(false)} 
+                    onYes={onDelete} />
                 : <></>}
         </div>
 }
