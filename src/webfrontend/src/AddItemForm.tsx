3ccebb52c422526d1d8204683f8b519614b3c3ad
brucengdev <brucengdev@gmail.com>
2025-09-05 07:30:19 +0700

    Added later checkbox

--- src/webfrontend/src/AddItemForm.tsx
+++ src/webfrontend/src/AddItemForm.tsx
@@ -70,6 +70,10 @@ export function AddItemForm(props: AddItemFormProps) {
             Done
             <input type="checkbox" />
         </label>
+        <label>
+            Later
+            <input type="checkbox" />
+        </label>
         <div className="flex justify-end gap-2">
             <Button 
                 mode={ButtonMode.PRIMARY}
