a261eb574c4dac7f92a4e7822e2fa22ece65ca1e
Nguyen Nguyen <88577724+brucengdev@users.noreply.github.com>
2025-09-05 07:54:08 +0700

    Add migration. (#35)

---
+++ src/Backend.WebApi/Migrations/20250905005005_AddDoneAndLater.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDoneAndLater : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Done",
+                table: "Items",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Later",
+                table: "Items",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Done",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Later",
+                table: "Items");
+        }
+    }
+}
