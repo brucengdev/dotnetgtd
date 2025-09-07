932f62addd47ed3918407ac723db224bd6d21154
Nguyen Nguyen <88577724+brucengdev@users.noreply.github.com>
2025-09-07 13:04:06 +0700

    Add left side task filters (#41)
    * Created task filters
    
    * Added no project filter
    
    * added no tag filter.
    
    * Show project filters.
    
    * Add tag filters.
    
    * Extract link cotnrol.
    
    * switch to use checkbox for filters.
    
    * Remove reference to unused control.
    
    * Add more filter toggles.

---
+++ src/webfrontend/src/TaskFilters.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { TaskFilters } from "./TaskFilters";
+import { TestClient } from "./__test__/TestClient";
+import '@testing-library/jest-dom'
+import { sleep } from "./__test__/testutils";
+
+describe("TaskFilters views", () => {
+    it("shows tag and project filters", async () => {
+        const client = new TestClient()
+        client.Projects = [
+            { id: 1, name: "Project 1", later: false },
+            { id: 2, name: "Project 2", later: false },
+        ]
+        client.Tags = [
+            { id: 1, name: "Tag 1" },
+            { id: 2, name: "Tag 2" },
+        ]
+        render(<TaskFilters client={client} />)
+        await sleep(1)
+
+        expect(screen.getByRole("checkbox", {name: "Active tasks"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "Inactive tasks"})).toBeInTheDocument()
+
+        expect(screen.getByRole("checkbox", {name: "Completed tasks"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "Uncompleted tasks"})).toBeInTheDocument()
+
+        expect(screen.getByRole("checkbox", {name: "All projects"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "No project"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "Project 1"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "Project 2"})).toBeInTheDocument()
+
+        expect(screen.getByRole("checkbox", {name: "All tags"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "No tag"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "Tag 1"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "Tag 2"})).toBeInTheDocument()
+    })
+})
---
+++ src/webfrontend/src/TaskFilters.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import { IClient } from "./api/Client"
+import { Project } from "./models/Project"
+import { Tag } from "./models/Tag"
+import { CheckBox } from "./controls/CheckBox"
+
+interface TaskFiltersProps {
+    client: IClient
+}
+export function TaskFilters(props: TaskFiltersProps) {
+    const { client } = props
+    const [projects, setProjects] = useState<Project[] | undefined>(undefined)
+    const [tags, setTags] = useState<Tag[] | undefined>(undefined)
+    if(projects === undefined) {
+        client.GetProjects()
+        .then(retrievedProjects => setProjects(retrievedProjects))
+    }
+    if(tags === undefined) {
+        client.GetTags()
+        .then(retrievedTags => setTags(retrievedTags))
+    }
+    return <div data-testId="task-filters">
+        <CheckBox label="Active tasks" checked={false} />
+        <CheckBox label="Inactive tasks" checked={false} />
+
+        <CheckBox label="Completed tasks" checked={false} />
+        <CheckBox label="Uncompleted tasks" checked={false} />
+
+        <CheckBox label="All projects" checked={false} />
+        <CheckBox label="No project" checked={false} />
+        {(projects || []).map(p => <CheckBox key={p.id} label={p.name} checked={false} />)}
+
+        <CheckBox label="All tags" checked={false} />
+        <CheckBox label="No tag" checked={false} />
+        {(tags || []).map(t => <CheckBox key={t.id} label={t.name} checked={false} />)}
+    </div>
+}
