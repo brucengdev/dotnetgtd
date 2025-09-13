a1aa2ae4a081f2b55b4c03ed4362c0a0ca5be671
brucengdev <brucengdev@gmail.com>
2025-09-13 20:19:52 +0700

    Added test for tag filter.

---
+++ src/Backend.WebApi.Tests/Repository/ItemRepositoryTests.GetItems.Tags.cs
@@ -0,0 +1,54 @@
+﻿namespace Backend.WebApi.Tests.Repository;
+
+public partial class ItemRepositoryTests
+{
+    internal static TestData TagTestData()
+    {
+        return new()
+        {
+            Items = [
+                new() { Id = 1, Description = "Task A", UserId = 1 },
+                new() { Id = 2, Description = "Task B", UserId = 1 },
+                new() { Id = 3, Description = "Task C", UserId = 1 },
+                new() { Id = 4, Description = "Task D", UserId = 1 }
+            ],
+            Tags = [
+                new() {Id = 1, UserId = 1, Name = "Tag A" },
+                new() {Id = 2, UserId = 1, Name = "Tag B" }
+            ],
+            ItemTagMappings = [
+                new() {Id = 1, ItemId = 1, TagId = 1},
+                new() {Id = 2, ItemId = 1, TagId = 2},
+                new() {Id = 3, ItemId = 4, TagId = 1}
+            ]
+        };
+    }
+
+    public static IEnumerable<object[]> TagFilterTests()
+    {
+        return new List<GetItemsCase>
+        {
+            new()
+            {
+                UserId = 1, TagIds = null,
+                ExpectedItemDescriptions = ["Task A", "Task B", "Task C", "Task D"]
+            }
+        }.Select(tc => tc.ToObjectArray());
+    }
+
+    [Theory]
+    [MemberData(nameof(TagFilterTests))]
+    public void GetItems_tag_filter_tests(
+        int userId,
+        IEnumerable<bool> completionStatuses,
+        IEnumerable<bool> laterStatuses,
+        IEnumerable<int?>? projectIds,
+        int[]? tagIds,
+        IEnumerable<string> expectedItemDescriptions)
+    {
+        var dbContext = CreateTestDB(TagTestData());
+        ExecuteGetItemTests(dbContext, 
+            userId, completionStatuses, laterStatuses, 
+            projectIds, tagIds, expectedItemDescriptions);
+    }
+}
