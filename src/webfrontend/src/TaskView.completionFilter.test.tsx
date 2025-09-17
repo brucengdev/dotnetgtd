d7541d0c1137858feed747334f8884d0bfbb34d3
brucengdev <brucengdev@gmail.com>
2025-09-17 12:39:41 +0700

    Get no tasks when both completed and uncompleted filters are inactive.

--- src/webfrontend/src/TaskView.completionFilter.test.tsx
+++ src/webfrontend/src/TaskView.completionFilter.test.tsx
@@ -82,4 +82,25 @@ describe("TaskView", () => {
         expect(items[1].querySelector('[data-testId="done"]')).toBeChecked()
         expect(items[1].querySelector('[data-testId="later"]')).not.toBeChecked()
     })
+
+    it("get no tasks when both filters are inactive", async () => {
+        const client = new TestClient()
+        client.Items = [
+            { id: 1, description: "Task A", projectId: 0, done: false, later: false },
+            { id: 2, description: "Task B", projectId: 0, done: true, later: false }
+        ]
+        render(<TaskView client={client} />)
+
+        await sleep(1)
+
+        screen.getByRole("checkbox", { name: "Uncompleted tasks"}).click()
+
+        await sleep(1)
+
+        expect(screen.getByRole("checkbox", { name: "Uncompleted tasks"})).not.toBeChecked()
+        expect(screen.getByRole("checkbox", { name: "Completed tasks"})).not.toBeChecked()
+
+        const items = screen.queryAllByTestId("item")
+        expect(items.length).toBe(0)
+    })
 })
