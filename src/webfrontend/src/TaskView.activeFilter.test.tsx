a9222779d75a7f80a44eb372a02458829cf954f8
brucengdev <brucengdev@gmail.com>
2025-09-18 12:46:50 +0700

    wip failed test.

--- src/webfrontend/src/TaskView.activeFilter.test.tsx
+++ src/webfrontend/src/TaskView.activeFilter.test.tsx
@@ -19,12 +19,12 @@ describe("TaskView", () => {
         expect(screen.getByRole("checkbox", { name: "Active tasks"})).toBeChecked()
         expect(screen.getByRole("checkbox", { name: "Inactive tasks"})).not.toBeChecked()
 
-        // const items = screen.queryAllByTestId("item")
-        // expect(items.length).toBe(1)
-        // expect(items[0].querySelector('[data-testId="description"]')?.textContent).toBe("Task A")
-        // expect(items[0].querySelector('[data-testId="project"]')?.textContent).toBe("")
-        // expect(items[0].querySelector('[data-testId="done"]')).not.toBeChecked()
-        // expect(items[0].querySelector('[data-testId="later"]')).not.toBeChecked()
+        const items = screen.queryAllByTestId("item")
+        expect(items.length).toBe(1)
+        expect(items[0].querySelector('[data-testId="description"]')?.textContent).toBe("Task A")
+        expect(items[0].querySelector('[data-testId="project"]')?.textContent).toBe("")
+        expect(items[0].querySelector('[data-testId="done"]')).not.toBeChecked()
+        expect(items[0].querySelector('[data-testId="later"]')).not.toBeChecked()
     })
 
     // it("get completed tasks", async () => {
