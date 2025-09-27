45def22ffea40e04ccf5b895f04ae6f23323cd1e
brucengdev <brucengdev@gmail.com>
2025-09-27 17:51:54 +0700

    Test GetProjects by userId.

---
+++ src/Backend.WebApi.Tests/Repository/ProjectRepositoryTests.GetItem.cs
@@ -0,0 +1,31 @@
+﻿using Backend.WebApi.Repository;
+using Shouldly;
+
+namespace Backend.WebApi.Tests.Repository;
+
+public partial class ProjectRepositoryTests
+{
+    [Fact]
+    public void TestGetProjectsByUser()
+    {
+        //arrange
+        var dbContext = Utils.CreateTestDB();
+        dbContext.Projects.AddRange(
+        [
+            new() { Id = 1, Name = "Project A", Later = false, UserId = 1 },
+            new() { Id = 2, Name = "Project B", Later = true, UserId = 1 },
+            new() { Id = 3, Name = "Project C", Later = true, UserId = 2 },
+            new() { Id = 4, Name = "Project D", Later = false, UserId = 2 }
+        ]);
+        dbContext.SaveChanges();
+        var sut = new ProjectRepository(dbContext);
+        
+        //act
+        var projects = sut.GetProjects(2, null, null);
+        
+        projects.ShouldBe([
+            new() { Id = 3, Name = "Project C", Later = true, UserId = 2 },
+            new() { Id = 4, Name = "Project D", Later = false, UserId = 2 }
+        ]);
+    }
+}
