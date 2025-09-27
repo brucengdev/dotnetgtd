7f62d801a1115284a3f955c657e945686487cf59
brucengdev <brucengdev@gmail.com>
2025-09-27 17:03:30 +0700

    Add filter checkboxes.

--- src/webfrontend/src/ProjectFilters.tsx
+++ src/webfrontend/src/ProjectFilters.tsx
@@ -1,5 +1,11 @@
+import { CheckBox } from "./controls/CheckBox";
 
 export function ProjectFilters() {
     return <div data-testid="project-filters">
+            <CheckBox label="Active projects" checked={false} />
+            <CheckBox label="Inactive projects" checked={false} />
+            
+            <CheckBox label="Completed projects" checked={false} />
+            <CheckBox label="Uncompleted projects" checked={false} />
         </div>
 }
