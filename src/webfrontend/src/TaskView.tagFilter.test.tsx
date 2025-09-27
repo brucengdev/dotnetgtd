435f84a99da1fb19d1aaeab0333237e5505c4bc5
brucengdev <brucengdev@gmail.com>
2025-09-27 12:50:18 +0700

    Show tasks from multiple selected tags

--- src/webfrontend/src/TaskView.tagFilter.test.tsx
+++ src/webfrontend/src/TaskView.tagFilter.test.tsx
@@ -97,38 +97,38 @@ describe("TaskView", () => {
         expect(items[1].querySelector('[data-testId="description"]')?.textContent).toBe("Task B")
     })
 
-    // it("shows tasks from multiple selected projects", async () => {
-    //     const client = new TestClient()
-    //     client.Items = [
-    //         { id: 1, description: "Task A", projectId: undefined,   done: false, later: false },
-    //         { id: 2, description: "Task B", projectId: 1,           done: false, later: false },
-    //         { id: 3, description: "Task C", projectId: 2,           done: false, later: false }
-    //     ]
-    //     client.Projects = [
-    //         { id: 1, name: "Project 1", later: false },
-    //         { id: 2, name: "Project 2", later: false },
-    //         { id: 3, name: "Project 3", later: false }
-    //     ]
-    //     render(<TaskView client={client} />)
+    it("shows tasks from multiple selected tags", async () => {
+        const client = new TestClient()
+        client.Items = [
+            { id: 1, description: "Task A", done: false, later: false, tagIds: [1,2] },
+            { id: 2, description: "Task B", done: false, later: false, tagIds: [2] },
+            { id: 3, description: "Task C", done: false, later: false, tagIds: [] }
+        ]
+        client.Tags = [
+            { id: 1, name: "Tag 1" },
+            { id: 2, name: "Tag 2" },
+            { id: 3, name: "Tag 3" }
+        ]
+        render(<TaskView client={client} />)
 
-    //     await sleep(1)
+        await sleep(1)
 
-    //     screen.getByRole("checkbox", { name: "All projects"}).click()
-    //     screen.getByRole("checkbox", { name: "No project"}).click()
-    //     screen.getByRole("checkbox", { name: "Project 1"}).click()
-    //     screen.getByRole("checkbox", { name: "Project 2"}).click()
+        screen.getByRole("checkbox", { name: "All tags"}).click()
+        screen.getByRole("checkbox", { name: "No tag"}).click()
+        screen.getByRole("checkbox", { name: "Tag 1"}).click()
+        screen.getByRole("checkbox", { name: "Tag 2"}).click()
 
-    //     await sleep(1)
+        await sleep(1)
 
-    //     expect(screen.getByRole("checkbox", { name: "All projects"})).not.toBeChecked()
-    //     expect(screen.getByRole("checkbox", { name: "Project 1"})).toBeChecked()
-    //     expect(screen.getByRole("checkbox", { name: "Project 2"})).toBeChecked()
-    //     expect(screen.getByRole("checkbox", { name: "Project 3"})).not.toBeChecked()
-    //     expect(screen.getByRole("checkbox", { name: "No project"})).not.toBeChecked()
+        expect(screen.getByRole("checkbox", { name: "All tags"})).not.toBeChecked()
+        expect(screen.getByRole("checkbox", { name: "Tag 1"})).toBeChecked()
+        expect(screen.getByRole("checkbox", { name: "Tag 2"})).toBeChecked()
+        expect(screen.getByRole("checkbox", { name: "Tag 3"})).not.toBeChecked()
+        expect(screen.getByRole("checkbox", { name: "No tag"})).not.toBeChecked()
 
-    //     const items = screen.queryAllByTestId("item")
-    //     expect(items.length).toBe(2)
-    //     expect(items[0].querySelector('[data-testId="description"]')?.textContent).toBe("Task B")
-    //     expect(items[1].querySelector('[data-testId="description"]')?.textContent).toBe("Task C")
-    // })
+        const items = screen.queryAllByTestId("item")
+        expect(items.length).toBe(2)
+        expect(items[0].querySelector('[data-testId="description"]')?.textContent).toBe("Task A")
+        expect(items[1].querySelector('[data-testId="description"]')?.textContent).toBe("Task B")
+    })
 })
