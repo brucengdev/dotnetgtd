6604fc7eeb58f2d8762b17c510776d400c7c3102
brucengdev <brucengdev@gmail.com>
2025-09-29 08:09:14 +0700

    Add default project filter.

--- src/webfrontend/src/ProjectFilters.test.tsx
+++ src/webfrontend/src/ProjectFilters.test.tsx
@@ -4,15 +4,19 @@ import { ProjectFilters } from "./ProjectFilters";
 import '@testing-library/jest-dom'
 
 describe("ProjectFilters", () => {
-    it("has necessary ui components", () => {
+    it("has necessary ui components and use default filter", () => {
         render(<ProjectFilters />)
 
         expect(screen.getByTestId("project-filters")).toBeInTheDocument()
 
         expect(screen.getByRole("checkbox", {name: "Active projects"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "Active projects"})).toBeChecked()
         expect(screen.getByRole("checkbox", {name: "Inactive projects"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "Inactive projects"})).not.toBeChecked()
 
         expect(screen.getByRole("checkbox", {name: "Completed projects"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "Completed projects"})).not.toBeChecked()
         expect(screen.getByRole("checkbox", {name: "Uncompleted projects"})).toBeInTheDocument()
+        expect(screen.getByRole("checkbox", {name: "Uncompleted projects"})).toBeChecked()
     })
 })
--- src/webfrontend/src/ProjectFilters.tsx
+++ src/webfrontend/src/ProjectFilters.tsx
@@ -1,11 +1,26 @@
 import { CheckBox } from "./controls/CheckBox";
 
-export function ProjectFilters() {
+export interface ProjectFilter {
+    active?: boolean
+    uncompleted?: boolean
+}
+
+interface ProjectFiltersProps {
+    filter?: ProjectFilter
+}
+
+const defaultFilter: ProjectFilter = {
+    active: true,
+    uncompleted: true
+}
+
+export function ProjectFilters(props: ProjectFiltersProps) {
+    const filter = props.filter || defaultFilter
     return <div data-testid="project-filters">
-            <CheckBox label="Active projects" checked={false} />
+            <CheckBox label="Active projects" checked={filter.active ?? false} />
             <CheckBox label="Inactive projects" checked={false} />
             
             <CheckBox label="Completed projects" checked={false} />
-            <CheckBox label="Uncompleted projects" checked={false} />
+            <CheckBox label="Uncompleted projects" checked={filter.uncompleted ?? false} />
         </div>
 }
