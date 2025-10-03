f5f4fab2c60afd19713dc596757a233d19737740
brucengdev <brucengdev@gmail.com>
2025-10-03 12:41:19 +0700

    WIP change to edit mode when item is clicked on.

--- src/webfrontend/src/ItemView.update.test.tsx
+++ src/webfrontend/src/ItemView.update.test.tsx
@@ -2,18 +2,24 @@ import { describe, expect, it, vitest } from "vitest"
 import ItemView from "./ItemView"
 import { screen, render, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom'
+import { sleep } from "./__test__/testutils";
 
 describe("ItemView update form", () => {
-    it("changes to edit mode when clicked on", () => {
+    it("changes to edit mode when clicked on", async () => {
         render(<ItemView description="Task A" 
             projectName="ProjectX" tagNames={["tag1", "tag2"]}
             done={false} later={false} />)
 
+        expect(screen.queryByTestId("edit-description")).not.toBeInTheDocument()
+
         const itemView = screen.getByTestId("item")
         itemView.click()
 
+        await sleep(1)
+
         const input = screen.getByTestId("edit-description")
         expect(input).toBeInTheDocument()
+        expect(screen.queryByTestId("description")).not.toBeInTheDocument()
 
         expect(input).toHaveValue("Task A")
     })
