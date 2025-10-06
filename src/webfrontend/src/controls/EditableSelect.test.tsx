62e5321dc1a7cfb5ba8133fd7d0e1f60bea31cde
brucengdev <brucengdev@gmail.com>
2025-10-06 22:01:45 +0700

    Refactor.

--- src/webfrontend/src/controls/EditableSelect.test.tsx
+++ src/webfrontend/src/controls/EditableSelect.test.tsx
@@ -1,6 +1,6 @@
 import { EditableSelect } from "./EditableSelect"
-import { describe, expect, it } from "vitest"
-import { screen, render } from "@testing-library/react";
+import { describe, expect, it, vitest } from "vitest"
+import { screen, render, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom'
 import { sleep } from "../__test__/testutils";
 
@@ -8,7 +8,14 @@ describe("EditableSelect", () => {
     it(`Show display view initially`, () => {
         render(<EditableSelect 
             displayViewDataTestId="displayField" 
-            editViewDataTestId="editField" />)
+            editViewDataTestId="editField" 
+            options={[
+                { value: "1", text: "Option 1" },
+                { value: "2", text: "Option 2" },
+                { value: "3", text: "Option 3" }
+            ]}
+            selectedValue="1"    
+        />)
 
         expect(screen.getByTestId("displayField")).toBeInTheDocument()
         expect(screen.queryByTestId("editField")).not.toBeInTheDocument()
@@ -17,12 +24,41 @@ describe("EditableSelect", () => {
     it(`Shows edit view when display view is clicked`, async () => {
         render(<EditableSelect 
             displayViewDataTestId="displayField" 
-            editViewDataTestId="editField" />)
+            editViewDataTestId="editField" 
+            options={[
+                { value: "1", text: "Option 1" },
+                { value: "2", text: "Option 2" },
+                { value: "3", text: "Option 3" }
+            ]}
+            selectedValue="1"
+        />)
 
         screen.getByTestId("displayField").click()
         await sleep(1)
 
         expect(screen.getByTestId("editField")).toBeInTheDocument()
         expect(screen.queryByTestId("displayField")).not.toBeInTheDocument()
+    })
+
+    it("Executes callback when value is changed", async () => {
+        var fn = vitest.fn()
+        render(<EditableSelect 
+            displayViewDataTestId="displayField" 
+            editViewDataTestId="editField" 
+            onChange={fn} 
+            options={[
+                { value: "1", text: "Option 1" },
+                { value: "2", text: "Option 2" },
+                { value: "3", text: "Option 3" }
+            ]}
+            selectedValue="1"
+        />)
+
+        screen.getByTestId("displayField").click()
+        await sleep(1)
+
+        fireEvent.change(screen.getByTestId("editField"), { target: { value: 2 } })
+
+        expect(fn).toHaveBeenCalledWith("2")
     })
 })
--- src/webfrontend/src/controls/EditableSelect.tsx
+++ src/webfrontend/src/controls/EditableSelect.tsx
@@ -1,18 +1,18 @@
 import { useState } from "react"
-import { Select } from "./Select"
+import { Select, SelectProps } from "./Select"
 
-interface EditableSelectProps {
+interface EditableSelectProps extends SelectProps {
     value?: string
     displayViewDataTestId?: string
     editViewDataTestId?: string
 }
 
 export function EditableSelect(props: EditableSelectProps) {
-    const { value, displayViewDataTestId, editViewDataTestId } = props
+    const { value, displayViewDataTestId, editViewDataTestId, options, onChange, selectedValue } = props
     const [isEditing, setIsEditing] = useState(false)
     return <>
         {isEditing
-            ? <Select dataTestId={editViewDataTestId} options={[]} selectedValue="" />
+            ? <Select dataTestId={editViewDataTestId} options={options} selectedValue={selectedValue} onChange={onChange} />
             :<div data-testId={displayViewDataTestId} 
                 onClick={() => setIsEditing(true)}>
                 {value??""}
