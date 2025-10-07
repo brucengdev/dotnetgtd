1790d9f7c61b27ff4e91f875d89f4b0380ad1e13
brucengdev <brucengdev@gmail.com>
2025-10-07 08:25:27 +0700

    EditableMultiSelect shows edit view when clicked on.

--- src/webfrontend/src/controls/EditableMultiSelect.test.tsx
+++ src/webfrontend/src/controls/EditableMultiSelect.test.tsx
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest"
 import { screen, render } from "@testing-library/react";
 import '@testing-library/jest-dom'
 import { EditableMultiSelect } from "./EditableMultiSelect";
+import { sleep } from "../__test__/testutils";
 
 describe("EditableMultiSelect", () => {
     it(`shows display view initially`, () => {
@@ -18,5 +19,24 @@ describe("EditableMultiSelect", () => {
 
         expect(screen.getByTestId("displayField")).toBeInTheDocument()
         expect(screen.queryByTestId("editField")).not.toBeInTheDocument()
+    })
+
+    it(`shows edit view when display view is clicked`, async () => {
+        render(<EditableMultiSelect 
+            textViewDataTestId="displayField" 
+            selectDataTestId="editField" 
+            options={[
+                { value: "1", text: "Option 1" },
+                { value: "2", text: "Option 2" },
+                { value: "3", text: "Option 3" }
+            ]}
+            selectedValues={["1"]}
+        />)
+
+        screen.getByTestId("displayField").click()
+        await sleep(1)
+
+        expect(screen.getByTestId("editField")).toBeInTheDocument()
+        expect(screen.queryByTestId("displayField")).not.toBeInTheDocument()
     })
 })
--- src/webfrontend/src/controls/EditableMultiSelect.tsx
+++ src/webfrontend/src/controls/EditableMultiSelect.tsx
@@ -1,10 +1,17 @@
-import { MultiSelectProps } from "./MultiSelect";
+import { useState } from "react";
+import { MultiSelect, MultiSelectProps } from "./MultiSelect";
 
 interface EditableMultiSelectProps extends MultiSelectProps {
     textViewDataTestId?: string
 }
 
 export function EditableMultiSelect(props: EditableMultiSelectProps) {
-    const { selectedValues, textViewDataTestId } = props
-    return <div data-testId={textViewDataTestId} >{selectedValues.join(",") ?? ""}</div>
+    const { selectedValues, textViewDataTestId, options, selectDataTestId } = props
+    const [ isEditing, setIsEditing ] = useState(false)
+    return isEditing
+            ?<MultiSelect options={[]} selectedValues={[]} selectDataTestId={selectDataTestId} />
+            :<div data-testId={textViewDataTestId} 
+                onClick={() => setIsEditing(true)} >
+                {selectedValues.join(",") ?? ""}
+            </div>
 }
