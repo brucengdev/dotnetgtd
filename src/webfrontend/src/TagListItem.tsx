78b81105845ea06e2af39172b55fe2b2585dc1a2
brucengdev <brucengdev@gmail.com>
2025-10-13 08:19:57 +0700

    Test editing tag's name.

--- src/webfrontend/src/TagListItem.tsx
+++ src/webfrontend/src/TagListItem.tsx
@@ -2,17 +2,20 @@ import { useState } from "react"
 import { ConfirmDeleteView } from "./ConfirmDeleteView"
 import { Button } from "./controls/Button"
 import { Tag } from "./models/Tag"
+import { EditableTextView } from "./controls/EditableTextView"
 
 interface TagListItemProps {
     tag: Tag,
     onDelete?: () => void
+    onChange?: (tag: Tag) => void
 }
 export function TagListItem(props: TagListItemProps) {
     const { tag, onDelete } = props
     const { name } = tag
     const [showConfirmDelete, setShowConfirmDelete] = useState(false)
     return <div data-testid="tag"  className="grid grid-cols-2 mb-1">
-        <div data-testid="name">{name}</div>
+        <EditableTextView text={name} 
+            textViewTestId="name" editViewTestId="edit-name" />
         {!showConfirmDelete 
             && <Button text="Delete" 
                 onClick={() => setShowConfirmDelete(true)} /> }
