7da29531d7140aa8dfe1fc7729a493a4da73ee13
brucengdev <brucengdev@gmail.com>
2025-10-24 08:13:02 +0700

    More test for TestClient.

--- src/webfrontend/src/__test__/TestClient.test.tsx
+++ src/webfrontend/src/__test__/TestClient.test.tsx
@@ -8,9 +8,22 @@ describe("TestClient", () => {
             { id: 1, name: "Uncompleted project", done: false, later: false },
             { id: 2, name: "Completed project", done: true, later: false },
         ]
-        const projects = await client.GetProjects({ uncompleted: true});
+        const projects = await client.GetProjects({ uncompleted: true})
         expect(projects).toEqual([
             { id: 1, name: "Uncompleted project", done: false, later: false },
-        ]);
-    });
+        ])
+    })
+
+    it("should return both active and inactive tasks", async () => {
+        const client = new TestClient();
+        client.Items = [
+            { id: 1, description: "Active Task", projectId: 0, done: false, later: false },
+            { id: 2, description: "Inactive Task", projectId: 1, done: false, later: true }
+        ]
+        const items = await client.GetItems({ uncompleted: true, active: true, inactive: true })
+        expect(items).toEqual([
+            { id: 1, description: "Active Task", projectId: 0, done: false, later: false },
+            { id: 2, description: "Inactive Task", projectId: 1, done: false, later: true }
+        ])
+    })
 })
