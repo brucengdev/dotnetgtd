ba25d037a83bba88593fd25eac83a4641f7298b1
brucengdev <brucengdev@gmail.com>
2025-10-27 07:07:40 +0700

    Added import endpoint.

---
+++ src/Backend.WebApi.Tests/Controller/DataControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Backend.WebApi.ActionFilters;
+using Backend.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Shouldly;
+
+namespace Backend.WebApi.Tests.Controller;
+
+public class DataControllerTests
+{
+    [Fact]
+    public void Import_endpoint_config()
+    {
+        var method = Utils.GetMethod<DataController>(nameof(DataController.Import));
+        method.ShouldNotBeNull();
+
+        var attributes = method?.GetCustomAttributes(typeof(HttpPutAttribute), true);
+        attributes.Length.ShouldBeGreaterThan(0, "Must have HttpPutAttribute");
+        
+        var putAttr = attributes[0] as HttpPutAttribute;
+        putAttr.Template.ShouldBe("[action]");
+        
+        var secAttrs = method?.GetCustomAttributes(typeof(ServiceFilterAttribute<SecurityFilterAttribute>), true);
+        secAttrs.Length.ShouldBe(1, "Must require authorization");
+    }
+}
---
+++ src/Backend.WebApi/Controllers/DataController.cs
@@ -0,0 +1,17 @@
+﻿using Backend.WebApi.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Backend.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DataController:ControllerBase
+{
+    [HttpPut("[action]")]
+    [ServiceFilter<SecurityFilterAttribute>]
+    public ActionResult Import()
+    {
+        return Ok();
+    }
+}
