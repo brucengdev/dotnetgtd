4d8373ab3c9d1a90aee13751944d7684e768203b
brucengdev <brucengdev@gmail.com>
2025-10-27 07:30:50 +0700

    WIP added failed test.

---
+++ src/Backend.Core.Tests/Managers/DataManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using Backend.Core.Manager;
+using Backend.Core.Tests.Mocks;
+using Shouldly;
+
+namespace Backend.Core.Tests;
+
+public class DataManagerTests
+{
+    public void Import_must_delete_current_data()
+    {
+        //arrange
+        var data = new TestDataSource();
+        var itemRepo = new TestItemRepository(data);
+        var sut = new DataManager(itemRepo);
+        
+        //act
+        sut.Import(new(), 12);
+        
+        //assert
+        data.Items.ShouldBeEmpty();
+        data.ItemTagMappings.ShouldBeEmpty();
+        data.Projects.ShouldBeEmpty();
+        data.Tags.ShouldBeEmpty();
+    }
+}
