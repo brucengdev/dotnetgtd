242b8ee621f9661f95ad28a41a1640c6ea09a173
brucengdev <brucengdev@gmail.com>
2025-10-28 06:43:28 +0700

    Added more to TestDataSource.

--- src/Backend.Core/Manager/DataManager.cs
+++ src/Backend.Core/Manager/DataManager.cs
@@ -1,9 +1,14 @@
-﻿using Backend.Models;
+﻿using Backend.Core.Repository;
+using Backend.Models;
 
 namespace Backend.Core.Manager;
 
 public class DataManager:IDataManager
 {
+    public DataManager(IItemRepository itemRepo)
+    {
+        
+    }
     public void Import(UserData data, int userId)
     {
     }
