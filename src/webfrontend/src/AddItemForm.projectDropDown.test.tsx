602793f2f577e0da0c8a36da62bc3764eea56d57
brucengdev <brucengdev@gmail.com>
2025-10-31 18:04:36 +0700

    WIP test

---
+++ src/webfrontend/src/AddItemForm.projectDropDown.test.tsx
@@ -0,0 +1,52 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from 'vitest'
+import '@testing-library/jest-dom'
+import { AddItemForm } from "./AddItemForm";
+import { TestClient } from "./__test__/TestClient";
+import { sleep } from "./__test__/testutils";
+import { ProjectFilter } from "./ProjectFilters";
+
+describe("AddItemForm", () => {
+    const testCases = [
+        { 
+            filter: {} as ProjectFilter , 
+            expectedProjectNames: [
+                "[No project]", 
+                "Active uncompleted project",
+                "Active completed project",
+                "Inactive uncompleted project",
+                "Inactive completed project"
+            ] 
+        }
+    ]
+    testCases.forEach(({filter, expectedProjectNames}) => {
+        it("filter project dropdown list based on current task filters", async () => {
+            const client = new TestClient()
+            client.Projects = [
+                { id: 1, name: "Active uncompleted project",    later: false,   done: false }, 
+                { id: 2, name: "Active completed project",      later: false,   done: true },
+                { id: 3, name: "Inactive uncompleted project",  later: true,    done: false }, 
+                { id: 4, name: "Inactive completed project",    later: true,    done: true },
+            ]
+            render(<AddItemForm projectFilter={filter} client={client} onCancel={() => {}}/>)
+
+            await sleep(1)
+
+            expectedProjectNames.forEach(projectName => {
+                const projectOption = screen.getByRole("option", {name: projectName}) as HTMLOptionElement
+                expect(projectOption).toBeInTheDocument()
+                const matchedProject = client.Projects.find(p => p.name === projectName)
+                expect(projectOption.getAttribute("value")).toBe(matchedProject?.id.toString()?? "0")
+                expect(projectOption.selected).toBe(projectName === "[No project]"? true: false)
+            });
+
+            client.Projects
+            .filter(p => !expectedProjectNames.includes(p.name))
+            .forEach(project => {
+                const projectName = project.name
+                const projectOption = screen.queryByRole("option", {name: projectName}) as HTMLOptionElement
+                expect(projectOption).not.toBeInTheDocument()
+            })
+        })
+    })
+})
