5107bfca914e02f1a1a710f5a3e4344a975334bc
Nguyen Nguyen <88577724+brucengdev@users.noreply.github.com>
2025-11-01 06:31:32 +0700

    Only projects with uncompleted next action are highlighted. (#102)
    * Added more tests.
    
    * Extract highlight tests to separate file.
    
    * Fix.

---
+++ src/webfrontend/src/TaskFilters.projectHighlight.test.tsx
@@ -0,0 +1,46 @@
+import { describe, expect, it } from "vitest";
+import { TestClient } from "./__test__/TestClient";
+import { sleep } from "./__test__/testutils";
+import { TaskFilters } from "./TaskFilters";
+import '@testing-library/jest-dom'
+import { screen } from "@testing-library/dom";
+import { render } from "@testing-library/react";
+
+describe("TaskFilters", () => {
+    
+    it("highlight projects with no tasks with tags", async () => {
+        const client = new TestClient()
+        client.Projects = [
+            { id: 1, name: "Project 1", later: false, done: false},
+            { id: 2, name: "Project 2", later: false, done: false },
+            { id: 3, name: "Project 3", later: false, done: false }
+        ]
+        client.Tags = [
+            { id: 1, name: "Tag 1" },
+            { id: 2, name: "Tag 2" }
+        ]
+        client.Items = [
+            //project 1
+            { id: 1, description: "Item 1", done: false, later: false, projectId: 1, tagIds: [1] },
+            { id: 2, description: "Item 2", done: false, later: false, projectId: 1, tagIds: [] },
+
+            //project 2
+            { id: 3, description: "Item 3", done: false, later: false, projectId: 2, tagIds: [] },
+            { id: 4, description: "Item 4", done: false, later: false, projectId: 2, tagIds: [] },
+
+            //project 3
+            //completed action with tag should not count
+            { id: 5, description: "Item 5", done: true, later: false, projectId: 3, tagIds: [1] },
+        ]
+        render(<TaskFilters client={client} />)
+        await sleep(1)
+
+        expect(screen.getByRole("checkbox", {name: "Project 1"}).parentElement?.className).not.toContain("text-red-500")
+        
+        expect(screen.getByRole("checkbox", {name: "Project 2"}).parentElement?.className).toContain("text-red-500")
+
+        expect(screen.getByRole("checkbox", {name: "Project 3"}).parentElement?.className).toContain("text-red-500")
+    })
+
+
+})
