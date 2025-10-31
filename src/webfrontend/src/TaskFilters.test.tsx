3db0f350bdd8d6d1f6058a875893adf5a1d07d6d
brucengdev <brucengdev@gmail.com>
2025-11-01 06:15:10 +0700

    Added more tests.

--- src/webfrontend/src/TaskFilters.test.tsx
+++ src/webfrontend/src/TaskFilters.test.tsx
@@ -40,7 +40,8 @@ describe("TaskFilters views", () => {
         const client = new TestClient()
         client.Projects = [
             { id: 1, name: "Project 1", later: false, done: false},
-            { id: 2, name: "Project 2", later: false, done: false }
+            { id: 2, name: "Project 2", later: false, done: false },
+            { id: 3, name: "Project 3", later: false, done: false }
         ]
         client.Tags = [
             { id: 1, name: "Tag 1" },
@@ -48,16 +49,19 @@ describe("TaskFilters views", () => {
         ]
         client.Items = [
             { id: 1, description: "Item 1", done: false, later: false, projectId: 1, tagIds: [1] },
-            { id: 2, description: "Item 2", done: false, later: false, projectId: 2 },
+            { id: 2, description: "Item 2", done: false, later: false, projectId: 1, tagIds: [] },
+
+            { id: 3, description: "Item 3", done: false, later: false, projectId: 2, tagIds: [] },
+            { id: 4, description: "Item 4", done: false, later: false, projectId: 2, tagIds: [] },
         ]
         render(<TaskFilters client={client} />)
         await sleep(1)
 
-        expect(screen.getByRole("checkbox", {name: "Project 1"})).toBeInTheDocument()
         expect(screen.getByRole("checkbox", {name: "Project 1"}).parentElement?.className).not.toContain("text-red-500")
         
-        expect(screen.getByRole("checkbox", {name: "Project 2"})).toBeInTheDocument()
         expect(screen.getByRole("checkbox", {name: "Project 2"}).parentElement?.className).toContain("text-red-500")
+
+        expect(screen.getByRole("checkbox", {name: "Project 3"}).parentElement?.className).toContain("text-red-500")
     })
 
 
