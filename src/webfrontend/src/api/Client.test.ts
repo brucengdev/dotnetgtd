e2a5524f705ce7ed634098ed0bf8cec60084eb57
brucengdev <brucengdev@gmail.com>
2025-11-05 08:15:50 +0700

    Added test to verify Client use * when both completed and uncompleted are true.

---
+++ src/webfrontend/src/api/Client.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it, vitest } from "vitest";
+import { Client } from "./Client";
+
+describe("Client.GetItems", () => {
+    it("use * for completion filter if both completed and uncompleted are true", async () => {
+        const foo = vitest.spyOn(globalThis, "fetch").mockResolvedValueOnce(new Response(JSON.stringify([]), { status: 200 }));
+        const client = new Client()
+
+        client.GetItems({
+            completed: true,
+            uncompleted: true
+        })
+
+        expect(foo).toHaveBeenCalledWith(expect.stringContaining("complete=*"), expect.any(Object));
+        vitest.restoreAllMocks
+    })
+})
