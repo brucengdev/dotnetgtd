d6a5809ec1f38c20fb7db9f3dff737486b3130f3
brucengdev <brucengdev@gmail.com>
2025-11-07 12:00:30 +0700

    More test for Client.GetProjects.

--- src/webfrontend/src/api/Client.test.ts
+++ src/webfrontend/src/api/Client.test.ts
@@ -15,3 +15,19 @@ describe("Client.GetItems", () => {
         vitest.restoreAllMocks
     })
 })
+
+
+describe("Client.GetProjects", () => {
+    it("use * for completion filter if both completed and uncompleted are true", async () => {
+        const foo = vitest.spyOn(globalThis, "fetch").mockResolvedValueOnce(new Response(JSON.stringify([]), { status: 200 }));
+        const client = new Client()
+
+        client.GetProjects({
+            completed: true,
+            uncompleted: true
+        })
+
+        expect(foo).toHaveBeenCalledWith(expect.stringContaining("complete=*"), expect.any(Object));
+        vitest.restoreAllMocks
+    })
+})
