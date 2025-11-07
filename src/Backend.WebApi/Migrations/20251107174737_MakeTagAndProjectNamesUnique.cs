5272b0bd137c90db850c627d6c75c9533518c31e
Nguyen Nguyen <88577724+brucengdev@users.noreply.github.com>
2025-11-08 00:53:29 +0700

    Make Tag and Project names required to be unique. (#113)

---
+++ src/Backend.WebApi/Migrations/20251107174737_MakeTagAndProjectNamesUnique.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeTagAndProjectNamesUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tags",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Projects",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Name",
+                table: "Tags",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_Name",
+                table: "Projects",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_Name",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_Name",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tags",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
