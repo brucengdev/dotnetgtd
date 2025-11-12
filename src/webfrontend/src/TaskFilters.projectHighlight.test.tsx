f183f6f318277fc3ce668f82d29a337c0dfa5c33
brucengdev <brucengdev@gmail.com>
2025-11-12 22:53:13 +0700

    wip

--- src/webfrontend/src/TaskFilters.projectHighlight.test.tsx
+++ src/webfrontend/src/TaskFilters.projectHighlight.test.tsx
@@ -7,8 +7,8 @@ import { screen } from "@testing-library/dom";
 import { render } from "@testing-library/react";
 
 describe("TaskFilters", () => {
-    
-    it("highlight projects with no tasks with tags", async () => {
+    //a next action is a task with at least one tag
+    it("highlight projects with no next actions", async () => {
         const client = new TestClient()
         client.Projects = [
             { id: 1, name: "Project 1", later: false, done: false},
@@ -40,7 +40,20 @@ describe("TaskFilters", () => {
         AssertHighlightedProjectFilter("Project 3", true)
     })
 
+    
+    it("highlight projects with no next actions 2", async () => {
+        const projects = [
+            { id: 1, name: "Project 1", later: false, done: false },
+            { id: 2, name: "Project 2", later: false, done: false, numberOfNextActions: 1 },
+            { id: 3, name: "Project 3", later: false, done: false, numberOfNextActions: 2 }
+        ]
+        render(<TaskFilters projects={projects} />)
+        await sleep(1)
 
+        AssertHighlightedProjectFilter("Project 1", false)
+        AssertHighlightedProjectFilter("Project 2", true)
+        AssertHighlightedProjectFilter("Project 3", true)
+    })
 })
 
 export function AssertHighlightedProjectFilter(projectName: string, shouldBeHighlighted: boolean) {
