e1dd8af85aa6b546bd88abe660d1d0a65e906174
brucengdev <brucengdev@gmail.com>
2025-11-12 23:00:31 +0700

    Remove old test.

--- src/webfrontend/src/TaskFilters.projectHighlight.test.tsx
+++ src/webfrontend/src/TaskFilters.projectHighlight.test.tsx
@@ -8,39 +8,6 @@ import { render } from "@testing-library/react";
 
 describe("TaskFilters", () => {
     //a next action is a task with at least one tag
-    it("highlight projects with no next actions", async () => {
-        const client = new TestClient()
-        client.Projects = [
-            { id: 1, name: "Project 1", later: false, done: false},
-            { id: 2, name: "Project 2", later: false, done: false },
-            { id: 3, name: "Project 3", later: false, done: false }
-        ]
-        client.Tags = [
-            { id: 1, name: "Tag 1" },
-            { id: 2, name: "Tag 2" }
-        ]
-        client.Items = [
-            //project 1
-            { id: 1, description: "Item 1", done: false, later: false, projectId: 1, tagIds: [1] },
-            { id: 2, description: "Item 2", done: false, later: false, projectId: 1, tagIds: [] },
-
-            //project 2
-            { id: 3, description: "Item 3", done: false, later: false, projectId: 2, tagIds: [] },
-            { id: 4, description: "Item 4", done: false, later: false, projectId: 2, tagIds: [] },
-
-            //project 3
-            //completed action with tag should not count
-            { id: 5, description: "Item 5", done: true, later: false, projectId: 3, tagIds: [1] },
-        ]
-        render(<TaskFilters client={client} />)
-        await sleep(1)
-
-        AssertHighlightedProjectFilter("Project 1", false)
-        AssertHighlightedProjectFilter("Project 2", true)
-        AssertHighlightedProjectFilter("Project 3", true)
-    })
-
-    
     it("highlight projects with no next actions 2", async () => {
         const projects = [
             { id: 1, name: "Project 1", later: false, done: false },
