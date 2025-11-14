b6d7be97d9bc68a6618fcf352e9eab4dd59d8f30
brucengdev <brucengdev@gmail.com>
2025-11-15 04:09:25 +0700

    Added react-router.

--- src/webfrontend/src/main.tsx
+++ src/webfrontend/src/main.tsx
@@ -3,12 +3,24 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { Client } from './api/Client.ts'
 import { Storage } from './storage/Storage.ts'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 
 const client = new Client()
 const storage = new Storage()
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App client={client} storage={storage} />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <StrictMode>
+      <App client={client} storage={storage} />
+    </StrictMode>
+  }
+])
+
+
+const root = document.getElementById('root')!
+
+createRoot(root).render(
+  <RouterProvider router={router} />
 )
+
